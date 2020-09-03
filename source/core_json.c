5f8d5b822977ea5d0c39cf9759fc435a767483c0
Oscar Abrina <abrinao@amazon.com>
2020-09-03 12:17:32 -0700

    Add missing function parameters to skipScalars and skipLiteral

--- source/core_json.c
+++ source/core_json.c
@@ -536,6 +536,8 @@ static bool_ strnEq( const char * a,
  * @param[in] buf  The buffer to parse.
  * @param[in,out] start  The index at which to begin.
  * @param[in] max  The size of the buffer.
+ * @param[in] literal  The type of literal.
+ * @param[in] length  The length of the literal.
  *
  * @return true if the literal was present;
  * false otherwise.
@@ -909,6 +911,7 @@ static void skipObjectScalars( const char * buf,
  * @param[in] buf  The buffer to parse.
  * @param[in,out] start  The index at which to begin.
  * @param[in] max  The size of the buffer.
+ * @param[in] mode  The first character of an array '[' or object '{'.
  */
 static void skipScalars( const char * buf,
                          size_t * start,
@@ -929,7 +932,11 @@ static void skipScalars( const char * buf,
     }
 }
 
+/**
+ * @brief Determines the maximum level of nesting in a JSON document.
+ */
 #ifndef JSON_MAX_DEPTH
+    /* Default value for the maximum depth of a JSON document. */
     #define JSON_MAX_DEPTH    32
 #endif
 
