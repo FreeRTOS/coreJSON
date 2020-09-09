63a721779c4838e5ae238ad07fef5e137c6626f7
Oscar Michael Abrina <abrinao@amazon.com>
2020-09-09 14:55:43 -0700

    Move root CMakeLists.txt to test/CMakeLists.txt (#13)
    Aside from moving, this also adds a coverity_analysis target to be used for running coverity. CI workflow is updated to follow suit.

--- tools/cmock/create_test.cmake
+++ tools/cmock/create_test.cmake
@@ -12,7 +12,7 @@ function(create_test test_name
     add_custom_command(OUTPUT ${test_name}_runner.c
                   COMMAND ruby
                     ${CMOCK_DIR}/vendor/unity/auto/generate_test_runner.rb
-                    ${CMAKE_SOURCE_DIR}/tools/cmock/project.yml
+                    ${MODULE_ROOT_DIR}/tools/cmock/project.yml
                     ${test_src_absolute}
                     ${test_name}_runner.c
                   DEPENDS ${test_src}
