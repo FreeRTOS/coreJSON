8401d38bd1568908cef8d8aaa2fa110f0ce08d91
bradleysmith23 <74752142+bradleysmith23@users.noreply.github.com>
2024-03-13 14:33:10 -0600

    MISRA fixes (#153)
    * MISRA fixes
    
    * Uncrustify: triggered by comment.
    
    * Update doxygen comments
    
    * Formatting fixes
    
    * Uncrustify: triggered by comment.
    
    * Run github actions.
    
    * set coreJSON_ASSERT_DEFINED to 0
    
    * Update loop_invariants.patch
    
    * Update assert definition
    
    * Uncrustify: triggered by comment.
    
    * Run Github Actions
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: Soren Ptak <ptaksoren@gmail.com>

--- source/include/core_json.h
+++ source/include/core_json.h
@@ -30,6 +30,7 @@
 #ifndef CORE_JSON_H_
 #define CORE_JSON_H_
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
 
@@ -38,6 +39,15 @@
     extern "C" {
 #endif
 /* *INDENT-ON* */
+
+/**
+ *  @brief By default, has the stand behavior of assert() for
+ *  parameter checking. To swap out the assert(), define this
+ *  macro with the desired behavior.  */
+#ifndef coreJSON_ASSERT
+    #define coreJSON_ASSERT( expr )    assert( expr )
+#endif
+
 
 /**
  * @ingroup json_enum_types
